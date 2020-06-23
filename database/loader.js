c869207cc23323df6bc3bd5b2a30abb262eddc90
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-06-23 03:42:42 -0700

    fix loader script

--- database/loader.js
+++ database/loader.js
@@ -1,46 +1,51 @@
 const fs = require('fs');
 const { galleryItem, muralItem, aboutItem, eventItem, storeItem, contactItem, mailingListItem } = require('./');
 
-galleryItem.deleteMany({}, () => console.log('deleted gallery'));
-muralItem.deleteMany({}, () => console.log('deleted murals'));
+galleryItem.deleteMany({}, () => {
+  console.log('deleted gallery');
 
-fs.readFile('database/data/galleryData.csv', (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    let array = data.toString().split('\n');
+  fs.readFile('database/data/galleryData.csv', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      let array = data.toString().split('\n');
 
-    if (!array[array.length - 1]) {
-      array.pop();
+      if (!array[array.length - 1]) {
+        array.pop();
+      }
+
+      array.forEach(item => {
+        let object = JSON.parse(item);
+        galleryItem.create(object)
+          .catch(err => console.error(err));
+      })
+      console.log('uploaded to gallery');
     }
-    
-    array.forEach(item => {
-      let object = JSON.parse(item);
-      galleryItem.create(object)
-        .catch(err => console.error(err));
-    })
-  }
-})
+  })
+});
 
-console.log('uploaded to gallery');
+muralItem.deleteMany({}, () => {
+  console.log('deleted murals');
 
-fs.readFile('database/data/muralsData.csv', (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    let array = data.toString().split('\n');
+  fs.readFile('database/data/muralsData.csv', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      let array = data.toString().split('\n');
 
-    if (!array[array.length - 1]) {
-      array.pop();
+      if (!array[array.length - 1]) {
+        array.pop();
+      }
+
+      array.forEach(item => {
+        let object = JSON.parse(item);
+        muralItem.create(object)
+          .catch(err => console.error(err));
+      })
+      console.log('uploaded to murals');
     }
-    
-    array.forEach(item => {
-      let object = JSON.parse(item);
-      muralItem.create(object)
-        .catch(err => console.error(err));
-    })
-    
-  }
-})
+  })
+});
 
-console.log('uploaded to murals');
+
+
